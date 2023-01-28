eddf2ed4ea38be4ade90f2f0b50c55e1bff52ab5
mouhamed <coulibalymouhamed27@gmail.com>
2023-01-28 15:47:52 +0000

    first add project

---
+++ Exercice/Exercice/src/test/java/com/DI_Bootcamp_ADACI_2022/Exercice/ExerciceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DI_Bootcamp_ADACI_2022.Exercice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
